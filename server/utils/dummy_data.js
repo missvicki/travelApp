3fd4cff501e336fbb1d5757f5a34edd39779898c
Victor Nomwesigwa <nvbethany479@gmail.com>
2019-07-20 01:39:37 +0300

    add a travel service

---
+++ server/utils/dummy_data.js
@@ -0,0 +1,9 @@
+export default {
+  travels: [
+    { id: 1, name: "vicki", date: "2019/12/01", cost: 10000, bus: "UAB19S" },
+    { id: 2, name: "patrick", date: "2019/11/02", cost: 20000, bus: "UAE82S" },
+    { id: 3, name: "joy", date: "2019/12/01", cost: 10000, bus: "UAB19S" },
+    { id: 4, name: "lucky", date: "2019/11/02", cost: 20000, bus: "UAE82S" },
+    { id: 5, name: "hadassah", date: "2019/12/01", cost: 10000, bus: "UAB19S" }
+  ]
+};
